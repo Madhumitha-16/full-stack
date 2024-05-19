dd6e6c5c8435c4b7db1307d4a64519d094d143c7
Madhumitha-16 <madhu.br03@gmail.com>
2024-05-19 23:52:41 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,3 @@
-
-
-
 const express = require('express');
 const mysql = require('mysql2');
 const multer = require('multer');
@@ -10,42 +7,40 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 const app = express();
-const router = express.Router();
 app.use(cors()); // Enable CORS
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER,      
-    password: process.env.DB_PASSWORD,     
-    database: process.env.DB_NAME
-});
-console.log(process.env.DB_HOST)
-connection.connect(function (err) {
-    if (err) {
-        console.log("database connection failed", err);
-    } else {
-        console.log("database connection successful!!!!");
-    }
+// Create a connection pool
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-app.get('/', function(req, res, next) {
+console.log(process.env.DB_HOST);
+
+app.get('/', function (req, res, next) {
     res.send('hi');
 });
 
-app.get('/buys', function(req, res, next) {
+app.get('/buys', function (req, res, next) {
     let sql = `SELECT * FROM buyers`;
-    connection.query(sql, function(err, result) {
+    pool.query(sql, function (err, result) {
         if (err) {
             return res.status(500).send("Database query failed");
         }
         res.json(result);
     });
 });
+
 app.post('/upload', upload.single('image'), (req, res) => {
     const image = req.file.buffer;
-    console.log(image)
+    console.log(image);
     const sql = 'INSERT INTO images (data) VALUES (?)';
-    connection.query(sql, [image], (err, result) => {
+    pool.query(sql, [image], (err, result) => {
         if (err) throw err;
         res.send('Image uploaded!');
     });
@@ -53,21 +48,19 @@ app.post('/upload', upload.single('image'), (req, res) => {
 
 app.get('/image/:id', (req, res) => {
     const sql = 'SELECT data FROM images WHERE id = ?';
-    connection.query(sql, [req.params.id], (err, result) => {
+    pool.query(sql, [req.params.id], (err, result) => {
         if (err) throw err;
         if (result.length > 0) {
-            res.set('Content-Type', 'image/jpeg');;
+            res.set('Content-Type', 'image/jpeg');
             res.send(result[0].data);
         } else {
             res.status(404).send('Image not found');
         }
     });
 });
-app.use('/', router);
 
-const PORT =  process.env.PORT || 3307;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3307;
+app.listen(3307, () => {
     console.log(`Server running on port ${PORT}`);
 });
-console.log(process.env.PORT)
-
+console.log(process.env.PORT);

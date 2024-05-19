acb9b53fa37ec2054b3659c50285b4ce997f84fd
Madhumitha-16 <madhu.br03@gmail.com>
2024-05-19 22:21:20 +0530

    rollback

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,10 @@ const express = require('express');
 const mysql = require('mysql2');
 const multer = require('multer');
 require('dotenv').config();
-const cors = require('cors');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-
 const app = express();
 const router = express.Router();
-app.use(cors()); // Enable CORS
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST, 
@@ -25,11 +22,11 @@ connection.connect(function (err) {
     }
 });
 
-app.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.send('hi');
 });
 
-app.get('/buys', function(req, res, next) {
+router.get('/buys', function(req, res, next) {
     let sql = `SELECT * FROM buyers`;
     connection.query(sql, function(err, result) {
         if (err) {
@@ -38,31 +35,10 @@ app.get('/buys', function(req, res, next) {
         res.json(result);
     });
 });
-app.post('/upload', upload.single('image'), (req, res) => {
-    const image = req.file.buffer;
-    console.log(image)
-    const sql = 'INSERT INTO images (data) VALUES (?)';
-    connection.query(sql, [image], (err, result) => {
-        if (err) throw err;
-        res.send('Image uploaded!');
-    });
-});
 
-app.get('/image/:id', (req, res) => {
-    const sql = 'SELECT data FROM images WHERE id = ?';
-    connection.query(sql, [req.params.id], (err, result) => {
-        if (err) throw err;
-        if (result.length > 0) {
-            res.set('Content-Type', 'image/jpeg');;
-            res.send(result[0].data);
-        } else {
-            res.status(404).send('Image not found');
-        }
-    });
-});
 app.use('/', router);
 
-const PORT =  process.env.PORT || 3307;
+const PORT = process.env.PORT || 3307;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
